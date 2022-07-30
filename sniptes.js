7bea63d254c6deb85c54774b4ed6e158f8aa1d1b
Abdul Razzaq <39210426+algo-encoders@users.noreply.github.com>
2022-07-31 00:07:01 +0500

    Fix . Error

--- sniptes.js
+++ sniptes.js
@@ -3,5 +3,5 @@
 let all_t = document.querySelectorAll('.max-w-3xl [data-testid="generation-result"] pre');
 let get_txt = '';
 for (let name of all_t) {
-    get_txt += name.innerText."|||";
+    get_txt += name.innerText+"|||";
 }
